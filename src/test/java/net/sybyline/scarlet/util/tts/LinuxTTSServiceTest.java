81084d10bc65f1dd7c76cfa63def8cbb312230ab
kubectl <me@kube.moe>
2025-09-17 21:09:11 +0200

    fix: cleanup the tests a little

--- src/test/java/net/sybyline/scarlet/util/tts/LinuxTTSServiceTest.java
+++ src/test/java/net/sybyline/scarlet/util/tts/LinuxTTSServiceTest.java
@@ -1,6 +1,8 @@
 package net.sybyline.scarlet.util.tts;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -8,15 +10,20 @@ import java.nio.file.Path;
 
 class LinuxTTSServiceTest {
     @Test
-    void demoTestMethod() throws IOException, VoiceProviderSynthException {
+    @EnabledOnOs(OS.LINUX)
+    void e2e_espeak_and_aplay() throws IOException, VoiceProviderSynthException {
         Path tmp = Files.createTempDirectory("tmp");
-        System.out.println(tmp.toString());
         VoiceProvider a = new EspeakVoiceProvider(tmp);
-        a.Synthesize("hello world", "english");
+        Path out = a.Synthesize("hello world", "default");
+        AplayAudioSink b = new AplayAudioSink();
+        b.play(out);
+        //noinspection ResultOfMethodCallIgnored
+        tmp.toFile().delete();
     }
 
     @Test
-    void testLinuxTTSService() throws IOException {
+    @EnabledOnOs({OS.LINUX, OS.WINDOWS})
+    void test_e2e_TTSFactory() throws IOException {
         Path tmp = Files.createTempDirectory("tmp");
         System.out.println(tmp.toString());
         TTSService a = TTSServiceFactory.build(tmp);
