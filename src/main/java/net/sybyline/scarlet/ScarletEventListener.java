1cdbd2e2dac91b7ebffeb902e7b133a18227dab3
kubectl <me@kube.moe>
2025-09-17 20:45:26 +0200

    fix: add missing changes to the ScarletEventListener

--- src/main/java/net/sybyline/scarlet/ScarletEventListener.java
+++ src/main/java/net/sybyline/scarlet/ScarletEventListener.java
@@ -33,11 +33,11 @@ import io.github.vrchatapi.model.User;
 import net.sybyline.scarlet.ext.AvatarSearch;
 import net.sybyline.scarlet.util.MiscUtils;
 import net.sybyline.scarlet.util.Pacer;
-import net.sybyline.scarlet.util.TTSService;
+import net.sybyline.scarlet.util.OldTTSService;
 import net.sybyline.scarlet.util.VersionedFile;
 import net.sybyline.scarlet.util.VrcIds;
 
-public class ScarletEventListener implements ScarletVRChatLogs.Listener, TTSService.Listener
+public class ScarletEventListener implements ScarletVRChatLogs.Listener, OldTTSService.Listener
 {
 
     public ScarletEventListener(Scarlet scarlet)
@@ -572,7 +572,7 @@ public class ScarletEventListener implements ScarletVRChatLogs.Listener, TTSServ
     // TTSService.Listener
 
     @Override
-    public void tts_init(TTSService tts)
+    public void tts_init(OldTTSService tts)
     {
     }
 
